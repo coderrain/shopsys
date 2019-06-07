d4eecbdc12999989209d4a4eddf0ad8333cb4ecc
coderrain <413842590@qq.com>
2019-06-07 15:08:57 +0800

    固定数据格式

--- api/index.js
+++ api/index.js
@@ -4,9 +4,21 @@ const fs = require('fs');
 const fsStore = require('../tools/fsStore')
 
 router.get('/shopMark', (req,res,next)=>{
-    let query = req.query
+    /*let query = req.query
     fsStore.fetch('./sql/shop.json',query).then(data=>{
         res.json(data)
+    })*/
+    let query = req.query;
+    let page = query.page || 1;
+    let pageSize = query.pageSize || 1000;
+    fsStore.fetch('./sql/shop.json').then(result=>{
+        let {code, data} = result;
+        data = data.slice((page-1)*pageSize,page*pageSize);
+        console.log((page-1)*pageSize ,page*pageSize)
+        res.json({
+            code:1,
+            data
+        })
     })
 })
 
@@ -24,6 +36,23 @@ router.get('/shopList', (req,res,next)=>{
     })
 })
 
+router.get('/shopListPage', (req,res,next)=>{
+    let query = req.query;
+    let page = query.page || 1;
+    let pageSize = query.pageSize || 1000;
+    /*delete query.page;
+    delete query.pageSize;*/
+    fsStore.fetch('./sql/shopItem.json').then(result=>{
+        let {code, data} = result;
+        data = data.slice((page-1)*pageSize ,page*pageSize);
+        console.log((page-1)*pageSize ,page*pageSize)
+        res.json({
+            code:1,
+            data
+        })
+    })
+})
+
 
 router.get('/ad', (req,res,next)=>{
     let query = req.query;
@@ -31,5 +60,8 @@ router.get('/ad', (req,res,next)=>{
         res.json(data)
     })
 })
+
+
+
 
 module.exports = router;
