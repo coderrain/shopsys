baf8891bfe43ab428e484503f098f8b4ec4335ab
coderrain <413842590@qq.com>
2019-06-11 12:21:56 +0800

    添加后台管理系统接口

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const app = express();
 const config = require('./config')
 const index = require('./api/index')
 const user = require('./api/user')
+const upload = require('./api/upload')
 const sessionMiddleware = require('./tools/session')
 const bodyParser = require('body-parser')
 
 app.use(sessionMiddleware)
 app.use(bodyParser())
 
+app.use(express.static('./'))
+
 app.use('/api/',index)
 app.use('/api/user',user)
+app.use('/api/upload',upload)
 
 app.listen(config.port, ()=>{
     console.log(`服务启动了访问http://localhost:${config.port}`)
